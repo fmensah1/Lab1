a8c38c05729ee8788c742b09e8c34cc93bb9bd7b
fmensah1 <fmensah1@my.westga.edu>
2024-08-27 12:38:17 -0400

    Working on issue #1

--- Lab 1/src/edu/westga/cs1302/lab1/view/BillTextGenerator.java
+++ Lab 1/src/edu/westga/cs1302/lab1/view/BillTextGenerator.java
@@ -1,5 +1,40 @@
 package edu.westga.cs1302.lab1.view;
 
+import edu.westga.cs1302.lab1.model.Bill;
+import edu.westga.cs1302.lab1.model.BillItem;
+
 public class BillTextGenerator {
 
+	private Bill bill;
+	
+	public BillTextGenerator() {
+		this.bill = new Bill();
+	}
+	
+	/** Return a String containing the list of bill items and total for the bill.
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 * 
+	 * @return a String containing the list of bill items and total for the bill
+	 */
+	public String getText() {
+		String text = "ITEMS" + System.lineSeparator();
+		double subTotal = 0.0;
+		for (BillItem item : this.bill.getItems()) {
+			text += item.getName() + " - " + item.getAmount() + System.lineSeparator();
+			subTotal += item.getAmount();
+		}
+		
+		text += System.lineSeparator();
+		text += "SUBTOTAL - $" + subTotal + System.lineSeparator();
+		double tax = subTotal * 0.1;
+		double tip = subTotal * 0.2;
+		text += "TAX - $" + tax + System.lineSeparator();
+		text += "TIP - $" + tip + System.lineSeparator();
+		text += "TOTAL - $" + (subTotal + tip + tax);
+		
+		return text;
+	}
+	
 }
